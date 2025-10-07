be4c2f4023677fc6aceace30523f271ef0db7b55
JManuel Velasco <jmanuel.velasco@gmail.com>
2025-10-08 00:06:26 +0200

    red: testAdminApproveUser [FAIL: Post-condition: User status must be Approved.: 0 != 1]

--- supply-chain-tracker/sc/test/SupplyChain.t.sol
+++ supply-chain-tracker/sc/test/SupplyChain.t.sol
@@ -8,11 +8,16 @@ contract SupplyChainTest is Test {
     SupplyChain public supplyChain;
 
     // Cuentas de prueba predefinidas por Anvil
-    address constant ADMIN = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
-    address constant PRODUCER_ADDRESS = address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
-    address constant FACTORY_ADDRESS = address(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC);
-    address constant RETAILER_ADDRESS = address(0x90F79bf6EB2c4f870365E785982E1f101E93b906);
-    address constant CONSUMER_ADDRESS = address(0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65);
+    address constant ADMIN =
+        address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+    address constant PRODUCER_ADDRESS =
+        address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
+    address constant FACTORY_ADDRESS =
+        address(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC);
+    address constant RETAILER_ADDRESS =
+        address(0x90F79bf6EB2c4f870365E785982E1f101E93b906);
+    address constant CONSUMER_ADDRESS =
+        address(0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65);
 
     function setUp() public {
         // 1. Decirle a Foundry que el próximo msg.sender será el ADMIN predefinido (0xf39...)
@@ -55,6 +60,41 @@ contract SupplyChainTest is Test {
             keccak256(abi.encodePacked(user.role)),
             keccak256(abi.encodePacked("Producer")),
             "User role must be Producer."
+        );
+    }
+
+    function testAdminApproveUser() public {
+        address userToApprove = PRODUCER_ADDRESS;
+
+        // 1. Arrange: El usuario solicita el rol.
+        vm.prank(userToApprove);
+        supplyChain.requestUserRole("Producer");
+
+        // Verificamos que esté en Pending.
+        SupplyChain.User memory userBefore = supplyChain.getUserInfo(
+            userToApprove
+        );
+        assertEq(
+            uint8(userBefore.status),
+            uint8(SupplyChain.UserStatus.Pending),
+            "Pre-condition: User must be Pending."
+        );
+
+        // 2. Act: El Admin aprueba la solicitud.
+        vm.prank(ADMIN);
+        supplyChain.changeStatusUser(
+            userToApprove,
+            SupplyChain.UserStatus.Approved
+        );
+
+        // 3. Assert: Verificamos que el estado del usuario haya cambiado a Approved.
+        SupplyChain.User memory userAfter = supplyChain.getUserInfo(
+            userToApprove
+        );
+        assertEq(
+            uint8(userAfter.status),
+            uint8(SupplyChain.UserStatus.Approved),
+            "Post-condition: User status must be Approved."
         );
     }
 }
